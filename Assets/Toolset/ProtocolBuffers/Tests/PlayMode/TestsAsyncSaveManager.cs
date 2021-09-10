87f1a1a811022b446e31c98755b5456c68fae7e8
James Collins <jamescolrepo@gmail.com>
2021-09-09 21:15:32 -0700

    Added scaffolding for async save manager tests.

---
+++ Assets/Toolset/ProtocolBuffers/Tests/PlayMode/TestsAsyncSaveManager.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Toolset.ProtocolBuffers.Tests
+{
+    public class TestsAsyncSaveManager
+    {
+    }
+}
